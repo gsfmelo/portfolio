51176fb9691626480f9a5405fc95b1abe1d9e401
Geovanna Melo <133133216+gsfmelo@users.noreply.github.com>
2023-10-23 17:42:51 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const ativarMenu = document.querySelector('.fa-bars')
-const navMenu = document.querySelector('header .navegacao-primaria')
-
-ativarMenu.addEventListener('click', () => {
-    ativarMenu.classList.toggle('fa-x')
-    navMenu.classList.toggle('ativado')
-})
-
