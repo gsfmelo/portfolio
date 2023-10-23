a6a0d13ace4f85904335dae715ae13a8ed09032d
Geovanna Melo <133133216+gsfmelo@users.noreply.github.com>
2023-10-23 17:48:17 -0300

    Atualizações no portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// codigo para adicionar responsividade ao menu
+const ativarMenu = document.querySelector('.fa-bars')
+const navMenu = document.querySelector('header .navegacao-primaria')
+
+ativarMenu.addEventListener('click', () => {
+    ativarMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
+})
+
